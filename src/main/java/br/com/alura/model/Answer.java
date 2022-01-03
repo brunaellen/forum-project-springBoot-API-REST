391c39e9237ce75551b8ccf981e3dbf3b0e93e42
brunaellen <brunaellen.souza@hotmail.com>
2022-01-03 17:21:58 +0000

    adding a database(h2) / using JPA specification and hibernate implementation

--- src/main/java/br/com/alura/model/Answer.java
+++ src/main/java/br/com/alura/model/Answer.java
@@ -2,12 +2,25 @@ package br.com.alura.model;
 
 import java.time.LocalDateTime;
 
-public class Answer {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class Answer {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String message;
+  
+  @ManyToOne
   private Topic topic;
   private LocalDateTime dateOfCreation = LocalDateTime.now();
+  
+  @ManyToOne
   private User author;
   private Boolean solution = false;
 
