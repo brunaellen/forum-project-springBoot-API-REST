74c4abdb183bd51436f278a571a04c7f78177bc1
brunaellen <brunaellen.souza@hotmail.com>
2022-01-04 19:17:13 +0000

    adding test to getTopicsByCourseName() method

--- src/test/java/br/com/alura/service/TopicServiceTest.java
+++ src/test/java/br/com/alura/service/TopicServiceTest.java
@@ -3,7 +3,6 @@ package br.com.alura.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import br.com.alura.controller.dto.TopicDto;
+import br.com.alura.model.Course;
 import br.com.alura.model.Topic;
 import br.com.alura.repository.TopicRepository;
 
@@ -28,11 +28,10 @@ class TopicServiceTest {
   
   @Test
   void getAllTopics_shouldReturnAListOfAllTopicDto() {
-    List<Topic> listOfTopics = new ArrayList<>();
     Topic firstTopic = new Topic();
     Topic secondTopic = new Topic();
     firstTopic.setTitle("first title");
-    listOfTopics = Arrays.asList(firstTopic, secondTopic);
+    List<Topic> listOfTopics = Arrays.asList(firstTopic, secondTopic);
     
     when(topicRepository.findAll()).thenReturn(listOfTopics);
     List<TopicDto> listAllTopicsDto = topicService.getAllTopics();
@@ -41,4 +40,38 @@ class TopicServiceTest {
     assertThat(listAllTopicsDto.size()).isEqualTo(2);
     assertThat(listAllTopicsDto.get(0)).hasFieldOrPropertyWithValue("title", "first title");
   }
+  
+  @Test
+  void getTopicsByCourseName_GivenACourseName_shouldReturnAListOfAllTopicDto() {
+    Topic firstTopic = new Topic();
+    Topic secondTopic = new Topic();
+    firstTopic.setCourse(new Course("Spring Boot", "Programming"));
+    secondTopic.setCourse(new Course("Spring Boot", "Front-end"));
+    List<Topic> listOfTopics = Arrays.asList(firstTopic, secondTopic);
+    
+    when(topicRepository.findByCourse_Name("Spring Boot")).thenReturn(listOfTopics);
+    List<TopicDto> listTopicsDtoByCourseName = topicService.getTopicsByCourseName("Spring Boot");
+    assertThat(listTopicsDtoByCourseName).isNotNull();
+    assertThat(listTopicsDtoByCourseName.size()).isEqualTo(2);
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
