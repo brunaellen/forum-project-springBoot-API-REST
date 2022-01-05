4518301d9fb56a439801ce5d45b9111bf65d0598
brunaellen <brunaellen.souza@hotmail.com>
2022-01-05 21:46:15 +0000

    refactoring

--- src/main/java/br/com/alura/controller/form/TopicForm.java
+++ src/main/java/br/com/alura/controller/form/TopicForm.java
@@ -7,10 +7,19 @@ public class TopicForm {
   public String getTitle() {
     return title;
   }
+  public void setTitle(String title) {
+    this.title = title;
+  }
   public String getMessage() {
     return message;
   }
+  public void setMessage(String message) {
+    this.message = message;
+  }
   public String getCourseName() {
     return courseName;
+  }
+  public void setCourseName(String courseName) {
+    this.courseName = courseName;
   }
 }
