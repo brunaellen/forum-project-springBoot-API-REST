85b6ad241f6fd3c55c59ada133ed58b135192336
brunaellen <brunaellen.souza@hotmail.com>
2022-02-03 18:11:20 +0000

    refactoring

--- src/test/java/br/com/alura/model/AnswerTest.java
+++ src/test/java/br/com/alura/model/AnswerTest.java
@@ -11,6 +11,11 @@ class AnswerTest {
     Topic firstTopic = new Topic("title", "first message", new Course("first name", "first category"));
     Topic secondTopic = new Topic("second title ", "second message", new Course("second name", "second category"));
 
-    EqualsVerifier.simple().forClass(Answer.class).withPrefabValues(Topic.class, firstTopic, secondTopic).verify();
+    EqualsVerifier
+      .simple()
+      .forClass(Answer.class)
+      .withIgnoredFields("dateOfCreation")
+      .withPrefabValues(Topic.class, firstTopic, secondTopic)
+      .verify();
   }
 }
