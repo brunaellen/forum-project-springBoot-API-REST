cab1e4859e6228010aed962c840b8437f6c78e60
brunaellen <brunaellen.souza@hotmail.com>
2022-02-05 19:07:41 +0000

    adding pagination

--- src/main/java/br/com/alura/repository/TopicRepository.java
+++ src/main/java/br/com/alura/repository/TopicRepository.java
@@ -1,12 +1,12 @@
 package br.com.alura.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.model.Topic;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
 
-  List<Topic> findByCourse_Name(String courseName);
+  Page<Topic> findByCourse_Name(String courseName, Pageable pagination);
 }
