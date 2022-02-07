9e43ca58e61f0da11740789548a640d000350ef6
brunaellen <brunaellen.souza@hotmail.com>
2022-02-07 11:01:03 +0000

    remove unecessary annotation since Spring automatically enable it without need to declare the annotation

--- src/main/java/br/com/alura/ForumAluraApplication.java
+++ src/main/java/br/com/alura/ForumAluraApplication.java
@@ -2,10 +2,8 @@ package br.com.alura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
 public class ForumAluraApplication {
 
 	public static void main(String[] args) {
