fbff0fefbe93c3637b503587866770b526a1958a
brunaellen <brunaellen.souza@hotmail.com>
2022-02-07 10:30:13 +0000

    adding sorting feature

--- src/main/java/br/com/alura/controller/TopicController.java
+++ src/main/java/br/com/alura/controller/TopicController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,8 +43,10 @@ public class TopicController {
   private TopicRepository topicRepository;
   
   @GetMapping("/listAll")
-  public ResponseEntity<Page<TopicDto>> listAll(@RequestParam int pageNumber, @RequestParam int size) {
-    Pageable pagination = PageRequest.of(pageNumber, size);
+  public ResponseEntity<Page<TopicDto>> listAll(@RequestParam int pageNumber,
+      @RequestParam int size, @RequestParam String sorting) {
+    
+    Pageable pagination = PageRequest.of(pageNumber, size, Direction.DESC, sorting);
     
     Page<Topic> allTopics = topicRepository.findAll(pagination);
     
