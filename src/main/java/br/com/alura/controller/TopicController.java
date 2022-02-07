55b1cbe8ae22a999c8161e05c755ff34ffd8d7c6
brunaellen <brunaellen.souza@hotmail.com>
2022-02-07 11:29:16 +0000

    refactoring pagination

--- src/main/java/br/com/alura/controller/TopicController.java
+++ src/main/java/br/com/alura/controller/TopicController.java
@@ -9,6 +9,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,7 +43,9 @@ public class TopicController {
   private TopicRepository topicRepository;
   
   @GetMapping("/listAll")
-  public ResponseEntity<Page<TopicDto>> listAll(Pageable pagination) {
+  public ResponseEntity<Page<TopicDto>> listAll(@PageableDefault(sort = "id", 
+      direction = Direction.DESC, page = 0, size = 10) 
+    Pageable pagination) {
 
     Page<Topic> allTopics = topicRepository.findAll(pagination);
     
