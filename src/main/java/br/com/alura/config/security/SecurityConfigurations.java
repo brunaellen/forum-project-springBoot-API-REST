b1070b949551065641a1ae06439d4c328a9b0557
brunaellen <brunaellen.souza@hotmail.com>
2022-02-08 15:55:22 +0000

    adding security to the project. Step 5 - (auhtorizing endpoint /topics/listAll)

--- src/main/java/br/com/alura/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/config/security/SecurityConfigurations.java
@@ -1,6 +1,7 @@
 package br.com.alura.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
   //method for configuration of authorization
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests().antMatchers(HttpMethod.GET, "/topics/listAll").permitAll();
   }
 
 }
