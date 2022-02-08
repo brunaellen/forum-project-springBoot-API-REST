047a8ff052f532dc7750a28228d7c7cb69235250
brunaellen <brunaellen.souza@hotmail.com>
2022-02-08 10:59:34 +0000

    adding cache eviction

--- src/main/java/br/com/alura/controller/TopicController.java
+++ src/main/java/br/com/alura/controller/TopicController.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -124,6 +125,7 @@ public class TopicController {
   
   @DeleteMapping("/delete/{id}")
   @Transactional
+  @CacheEvict(value = "listOfTopics", allEntries = true)
   public ResponseEntity<?> delete(@PathVariable Long id) {
     Optional<Topic> topic = topicRepository.findById(id);
     
