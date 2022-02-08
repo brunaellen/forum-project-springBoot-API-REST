f7eb25130c03e4e416c92cb610cf97172f602c1f
brunaellen <brunaellen.souza@hotmail.com>
2022-02-08 15:40:09 +0000

    adding security to the project. Step 4 - (override configure method to configure authorization)

--- src/main/java/br/com/alura/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/config/security/SecurityConfigurations.java
@@ -1,11 +1,17 @@
 package br.com.alura.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+  
+  //method for configuration of authorization
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+  }
 
 }
