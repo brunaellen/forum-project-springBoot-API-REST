c39a6b662763b8b163b47fe5b16c829c18ae5761
brunaellen <brunaellen.souza@hotmail.com>
2022-02-09 10:45:41 +0000

    adding security to the project. Step 12 - (injecting authenticationService class)

--- src/main/java/br/com/alura/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/config/security/SecurityConfigurations.java
@@ -1,19 +1,25 @@
 package br.com.alura.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
   
+  @Autowired
+  private AuthenticationService authenticationService;
+  
   //method for configuration of authentication
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
   }
   
   //method for configuration of authorization
@@ -21,7 +27,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests()
       .antMatchers(HttpMethod.GET, "/topics/listAll").permitAll()
-      .anyRequest().authenticated();
+      .anyRequest().authenticated()
+      .and().formLogin();
   }
-
 }
