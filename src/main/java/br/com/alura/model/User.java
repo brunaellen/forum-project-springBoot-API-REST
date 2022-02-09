cdfb729d6966fb01b132d00edefd60bd01972e76
brunaellen <brunaellen.souza@hotmail.com>
2022-02-09 14:39:01 +0000

    adding security to the project. Step 15 - (adding Role class as a field of User's class and refactoring User methods)

--- src/main/java/br/com/alura/model/User.java
+++ src/main/java/br/com/alura/model/User.java
@@ -1,24 +1,32 @@
 package br.com.alura.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 public class User implements UserDetails {
-  
+
+  private static final long serialVersionUID = 1L;
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String email;
   private String password;
+  
+  @ManyToMany(fetch = FetchType.EAGER)
+  private List<Role> role = new ArrayList<>();
 
   public Long getId() {
     return id;
@@ -91,33 +99,31 @@ public class User implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    // TODO Auto-generated method stub
-    return null;
+    return this.role;
   }
 
   @Override
   public String getUsername() {
-    // TODO Auto-generated method stub
-    return null;
+    return email;
   }
 
   @Override
   public boolean isAccountNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isAccountNonLocked() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isCredentialsNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isEnabled() {
-    return false;
+    return true;
   }
 }
