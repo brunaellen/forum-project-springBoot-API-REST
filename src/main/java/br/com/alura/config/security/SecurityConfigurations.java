4f2aee54259715311f2ff2bdbf7fa86663fbade4
brunaellen <brunaellen.souza@hotmail.com>
2022-02-09 14:49:09 +0000

    adding security to the project. Step 16 - (adding configuration to make h2 database endpoint accessible)

--- src/main/java/br/com/alura/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/config/security/SecurityConfigurations.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,5 +30,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
       .antMatchers(HttpMethod.GET, "/topics/listAll").permitAll()
       .anyRequest().authenticated()
       .and().formLogin();
+  }
+  
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/h2-console/**");
   }
 }
