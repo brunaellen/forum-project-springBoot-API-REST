feb40e2bf968088c8467740c94f3cfd29afc2696
brunaellen <brunaellen.souza@hotmail.com>
2022-02-11 18:36:47 +0000

    adding authentication using JSON WEB TOKEN. Step 16 - adding configurations of method that generates token - (seeting expiration date for token)

--- src/main/java/br/com/alura/config/security/TokenService.java
+++ src/main/java/br/com/alura/config/security/TokenService.java
@@ -1,8 +1,8 @@
 package br.com.alura.config.security;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +11,20 @@ import io.jsonwebtoken.Jwts;
 
 @Service
 public class TokenService {
+  
+  @Value(value = "${forum.jwt.expirationTimeMilliseconds}")
+  private String expirationTimeMilliseconds;
 
   public String generateToken(Authentication authentication) {
     User userLoggedIn = (User) authentication.getPrincipal();
     Date generationDate = new Date();
+    Date expirationDate = new Date(generationDate.getTime() + Long.parseLong(expirationTimeMilliseconds));
     
     return Jwts.builder()
         .setIssuer("API of Alura Forum")
         .setSubject(userLoggedIn.getId().toString())
-        .setIssuedAt(generationDate);
+        .setIssuedAt(generationDate)
+        .setExpiration(expirationDate);
   }
   
 }
