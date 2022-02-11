671127a94532ea681429680b3e001880c0416870
brunaellen <brunaellen.souza@hotmail.com>
2022-02-11 09:54:18 +0000

    adding authentication using JSON WEB TOKEN. Step 12 - (add method to generate token)

--- src/main/java/br/com/alura/config/security/TokenService.java
+++ src/main/java/br/com/alura/config/security/TokenService.java
@@ -1,5 +1,15 @@
 package br.com.alura.config.security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+
+@Service
 public class TokenService {
 
+  public String generateToken(Authentication authentication) {
+    return Jwts.builder();
+  }
+  
 }
