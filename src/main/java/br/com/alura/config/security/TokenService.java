e1fb6510211a9afad54ac00389215ed234b6d004
brunaellen <brunaellen.souza@hotmail.com>
2022-02-11 10:15:57 +0000

    adding authentication using JSON WEB TOKEN. Step 14 - adding configurations of method that generates token - (adding who owns the token)

--- src/main/java/br/com/alura/config/security/TokenService.java
+++ src/main/java/br/com/alura/config/security/TokenService.java
@@ -3,14 +3,18 @@ package br.com.alura.config.security;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import br.com.alura.model.User;
 import io.jsonwebtoken.Jwts;
 
 @Service
 public class TokenService {
 
   public String generateToken(Authentication authentication) {
+    User userLoggedIn = (User) authentication.getPrincipal();
+    
     return Jwts.builder()
-        .setIssuer("API of Alura Forum");
+        .setIssuer("API of Alura Forum")
+        .setSubject(userLoggedIn.getId().toString());
   }
   
 }
