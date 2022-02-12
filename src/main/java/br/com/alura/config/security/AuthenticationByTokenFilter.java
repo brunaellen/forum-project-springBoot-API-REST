f727e748ded0d8ce6980a28a5914f48d8695194e
brunaellen <brunaellen.souza@hotmail.com>
2022-02-12 19:14:46 +0000

    adding authentication using JSON WEB TOKEN. Step 21 - (adding method to recover token)

--- src/main/java/br/com/alura/config/security/AuthenticationByTokenFilter.java
+++ src/main/java/br/com/alura/config/security/AuthenticationByTokenFilter.java
@@ -10,5 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class AuthenticationByTokenFilter extends OncePerRequestFilter{
+  
+  private final String HEADER = "Authorization";
+  private final String TYPE = "Bearer ";
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+  }
+
+  private String recoverToken(HttpServletRequest request) {
+    String token = request.getHeader(HEADER);
+    
+    if(token == null || token.isEmpty() || !token.startsWith(TYPE)) {
+      return null;
+    }
+    return token.substring(TYPE.length(), token.length());
+  }
 
 }
