358c56ca6f259935372505035463cd5991767f89
brunaellen <brunaellen.souza@hotmail.com>
2022-02-12 18:53:09 +0000

    adding authentication using JSON WEB TOKEN. Step 20 - (adding class to have the logic to intercept requests, recover and validate the token and determine if the client has permissions or not)

---
+++ src/main/java/br/com/alura/config/security/AuthenticationByTokenFilter.java
@@ -0,0 +1,14 @@
+package br.com.alura.config.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class AuthenticationByTokenFilter extends OncePerRequestFilter{
+
+}
