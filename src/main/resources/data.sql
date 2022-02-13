5b1179cee4b3b03da20d4b340e549299b3a8c39e
brunaellen <brunaellen.souza@hotmail.com>
2022-02-13 19:13:30 +0000

    adding some values to data tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,15 @@
 INSERT INTO USER(name, email, password) VALUES('user', 'user@email.com', '$2a$10$TvpIywTmnZCE.5Ni6h.PSuknszA1YfA4.EeN9AOjGsV9qYHgrLgUm');
+INSERT INTO USER(name, email, password) VALUES('Bruna', 'bruna@email.com', '$2a$10$TvpIywTmnZCE.5Ni6h.PSuknszA1YfA4.EeN9AOjGsV9qYHgrLgUm');
 
 INSERT INTO COURSE(name, category) VALUES('Spring Boot', 'Programming');
 INSERT INTO COURSE(name, category) VALUES('HTML 5', 'Front-end');
 
+INSERT INTO ROLE(id, name) VALUES(1, 'USER');
+INSERT INTO ROLE(id, name) VALUES(2, 'ADMIN');
+
 INSERT INTO TOPIC(title, message, date_of_criation, status, author_id, course_id) VALUES('query', 'Error when creating a project', '2021-05-05 18:00:00', 'NOT_ANSWERED', 1, 1);
 INSERT INTO TOPIC(title, message, date_of_criation, status, author_id, course_id) VALUES('query 2', 'Project is not compiling', '2021-05-05 19:00:00', 'NOT_ANSWERED', 1, 1);
 INSERT INTO TOPIC(title, message, date_of_criation, status, author_id, course_id) VALUES('query 3', 'Tag HTML', '2021-05-05 20:00:00', 'NOT_ANSWERED', 1, 2);
+
+INSERT INTO USER_ROLE(user_id, role_id) VALUES(1, 1);
+INSERT INTO USER_ROLE(user_id, role_id) VALUES(2, 2);
