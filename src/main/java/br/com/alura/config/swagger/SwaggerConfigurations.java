4e6e0942a2d854bee2a8f778b8cb78e82f54e215
brunaellen <brunaellen.souza@hotmail.com>
2022-02-14 22:33:40 +0000

    Adding API documentation using Swagger specification and Springfox implementation. Step 5 - (adding configurations to send JWT through swagger)

--- src/main/java/br/com/alura/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/config/swagger/SwaggerConfigurations.java
@@ -1,11 +1,15 @@
 package br.com.alura.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.alura.model.User;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,6 +23,15 @@ public class SwaggerConfigurations {
         .apis(RequestHandlerSelectors.basePackage("br.com.alura"))
         .paths(PathSelectors.ant("/**"))
         .build()
-        .ignoredParameterTypes(User.class);
+        .ignoredParameterTypes(User.class)
+        .globalOperationParameters(
+            Arrays.asList(
+              new ParameterBuilder()
+              .name("Authorization")
+              .description("Header para Token JWT")
+              .modelRef(new ModelRef("string"))
+              .parameterType("header")
+              .required(false)
+              .build()));
   }
 }
