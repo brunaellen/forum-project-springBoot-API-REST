f53aaca86bfec4621ef183bf6c6fe7f58936a9b4
brunaellen <brunaellen.souza@hotmail.com>
2022-02-16 10:51:47 +0000

    setting configuration for the CourseRepositoryTest class, disabling auto configuration so I can  use my own configurations for the test(I have setup the database mode to be empty)

--- src/test/java/br/com/alura/repository/CourseRepositoryTest.java
+++ src/test/java/br/com/alura/repository/CourseRepositoryTest.java
@@ -6,19 +6,30 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import br.com.alura.model.Course;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class CourseRepositoryTest {
   
   @Autowired
   private CourseRepository courseRepository;
+  
+  @Autowired
+  private TestEntityManager entityManager;
 
   @Test
   void findByName_givenAName_ShouldReturnARegisteredCourse() {
     String courseName = "HTML 5";
+    Course htmlCourse = new Course(courseName, "programming");
+    entityManager.persist(htmlCourse);
+    
     Optional<Course> course = courseRepository.findByName(courseName);
     
     assertThat(course).isNotNull();
